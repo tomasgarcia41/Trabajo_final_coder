4b82dfa831f56f669da6ddadaf5c13eee0c1f884
Geronimo Benedetti <geronimobenedetti@MacBook-Pro-de-Geronimo.local>
2022-11-09 22:03:27 -0300

    Cree las vistas de blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.admin import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from blog.models import Post
+from django.views import View
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 
-# Create your views here.
+
+
+def index(request):
+    posts = Post.objects.order_by('-date_published').all()
+    return render(request, 'blog/index.html', {"posts": posts})
+
+def about(request):
+    return render(request, "blog/about.html")
+
+class ListPost(ListView):
+    paginate_by = 2
+    model = Post
+
+class CreatePost(CreateView):
+    model=Post
+    fields = ['title', 'short_content', 'content', 'image']
+    success_url = reverse_lazy("list-post")
+    
+class DetailPost(DetailView):
+    model=Post
+
+class UpdatePost(UpdateView):
+    model = Post
+    fields = ['title', 'short_content', 'content', 'image']
+    success_url = reverse_lazy("list-post")
+
+class DeletePost(DeleteView):
+    model = Post
+    success_url = reverse_lazy("list-post")
+
+class SearchPostByName(ListView):
+    def get_queryset(self):
+        blog_title = self.request.GET.get('post-title')
+        return Post.objects.filter(title__icontains=blog_title)
+
+class BlogLogin(LoginView):
+    template_name = 'blog/blog_login.html'
+    next_page = reverse_lazy("list-post")
+
+class BlogLogout(LogoutView):
+    template_name = 'blog/blog_logout.html'
+    
+class BlogSignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("blog-login")
+    template_name = "registration/signup.html"
+
+class ProfileUpdate(UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    success_url = reverse_lazy("blog-login")
